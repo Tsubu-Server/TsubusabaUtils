0bfc013f86f2c891777865673f4c0915f1563f70
Doncha0003 <tyat355962@gmail.com>
2025-10-17 15:16:56 +0900

    ワールド間移動の修正

--- src/main/java/net/tsubu/tsubusabautils/command/WorldTeleportCommand.java
+++ src/main/java/net/tsubu/tsubusabautils/command/WorldTeleportCommand.java
@@ -38,21 +38,27 @@ public class WorldTeleportCommand implements CommandExecutor {
 
         Location lastLoc = manager.getLastLocationByPrefix(player, targetWorldName);
         Location targetLoc;
+
         if (lastLoc != null && lastLoc.getWorld() != null && lastLoc.getWorld().getName().toLowerCase().startsWith(targetWorldName.toLowerCase())) {
             targetLoc = lastLoc.clone();
+            targetLoc.setX(targetLoc.getBlockX() + 0.5);
+            targetLoc.setZ(targetLoc.getBlockZ() + 0.5);
         } else {
             World targetWorld = Bukkit.getWorld(targetWorldName);
+            if (targetWorld == null) {
+                player.sendMessage("§cワールド '" + targetWorldName + "' が存在しません。");
+                return true;
+            }
             targetLoc = targetWorld.getSpawnLocation().clone();
+            targetLoc.setX(targetLoc.getBlockX() + 0.5);
+            targetLoc.setZ(targetLoc.getBlockZ() + 0.5);
+            targetLoc.setY(targetWorld.getHighestBlockYAt(targetLoc) + 1);
         }
-
-        targetLoc.setX(targetLoc.getBlockX() + 0.5);
-        targetLoc.setZ(targetLoc.getBlockZ() + 0.5);
-        targetLoc.setY(targetLoc.getWorld().getHighestBlockYAt(targetLoc) + 1);
 
         player.sendMessage("§7転送準備中...");
 
         Bukkit.getScheduler().runTask(manager.getPlugin(), () -> {
-            player.teleport(targetLoc);
+            player.teleport(targetLoc); // 同期テレポート
             player.sendMessage("§a" + targetLoc.getWorld().getName() + " に移動しました！");
         });
 
